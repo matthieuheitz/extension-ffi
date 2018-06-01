aa8e392b41244e282df0f239094cb5a2871be818
Matthieu Heitz <heitz.matthieu@gmail.com>
2018-06-01 14:49:14 +0200

    Don't use the module

--- adjmat1dmul/test.py
+++ adjmat1dmul/test.py
@@ -1,19 +1,10 @@
 import torch
-import torch.nn as nn
 from torch.autograd import Variable
 
-from modules.add import MyAddModule
+from functions.add import MyAddFunction
 
-class MyNetwork(nn.Module):
-    def __init__(self):
-        super(MyNetwork, self).__init__()
-        self.add = MyAddModule()
 
-    def forward(self, input1, input2):
-        return self.add(input1, input2)
-
-model = MyNetwork()
 x = torch.range(1, 25).view(5, 5)
+print(x)
 input1, input2 = Variable(x), Variable(x * 4)
-print(model(input1, input2))
-print(input1 + input2)
+print(MyAddFunction()(input1, input2))
